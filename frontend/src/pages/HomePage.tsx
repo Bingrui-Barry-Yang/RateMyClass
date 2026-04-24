50c73487095ec051901f1fd586507d3fc2e8c917
Mia22bms <shenyuanrong469@gmail.com>
2026-04-24 00:00:27 -0700

    Set up React Router and create HomePage skeleton

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-orange-600">
+        RateMyClass @ Oxy
+      </h1>
+    </div>
+  )
+}
+
+export default HomePage
